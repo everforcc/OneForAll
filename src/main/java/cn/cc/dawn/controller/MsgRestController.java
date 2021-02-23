45dc649658a9b61419e93e2a49bda8c84e13e311
everforcc <718497737@qq.com>
2021-02-23 14:41:34 +0800

    调整fun.html代码

--- src/main/java/cn/cc/dawn/controller/MsgRestController.java
+++ src/main/java/cn/cc/dawn/controller/MsgRestController.java
@@ -2,9 +2,13 @@ package cn.cc.dawn.controller;
 
 import cn.cc.dawn.jdkcraw.Bilibili_Cover;
 import cn.cc.dawn.utils.Init;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Yukino
@@ -32,6 +36,19 @@ public class MsgRestController {
 
         System.out.println(num);
         return bilibili_cover.getBilCover(num);
+    }
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file){
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get("E:\\audio\\"+file.getOriginalFilename());
+            Files.write(path,bytes);
+            return "文件上传成功";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "文件上传失败";
+        }
     }
 
 }
