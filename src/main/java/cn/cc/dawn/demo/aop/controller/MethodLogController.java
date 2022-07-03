7c42523205c3e64f6c1cc88e3315336330dbbf58
everforcc <718497737@qq.com>
2022-07-03 15:05:17 +0800

    2022-07-03 调整http文档和代码目录结构保持一致

---
+++ src/main/java/cn/cc/dawn/demo/aop/controller/MethodLogController.java
@@ -0,0 +1,29 @@
+package cn.cc.dawn.demo.aop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author everforcc 2021-09-25
+ * aop测试
+ */
+@RequestMapping(value = "/demo/alllog1")
+@RestController
+@Slf4j
+public class MethodLogController {
+
+    @GetMapping("/taop1/{id}")
+    public void tAOPLog1(@PathVariable("id")int id){
+        log.info("tAOPLog1(AllLogMethod)");
+    }
+
+    @GetMapping("/taop2/{id}")
+    public int tAOPLog(@PathVariable("id")int id){
+        log.info("tAOPLog(AllLogMethod)");
+        return id;
+    }
+
+}
