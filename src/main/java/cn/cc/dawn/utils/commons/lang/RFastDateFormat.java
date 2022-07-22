4fde5f8c297a0c53c8de021468e12628ef7ec693
everforcc <718497737@qq.com>
2022-07-22 15:40:48 +0800

    2022-07-22 日期格式校验

--- src/main/java/cn/cc/dawn/utils/commons/lang/RFastDateFormat.java
+++ src/main/java/cn/cc/dawn/utils/commons/lang/RFastDateFormat.java
@@ -9,6 +9,7 @@ package cn.cc.dawn.utils.commons.lang;
 import cn.cc.dawn.utils.constant.DateFormatConstant;
 import org.apache.commons.lang3.time.FastDateFormat;
 
+import java.text.ParseException;
 import java.util.Date;
 
 public class RFastDateFormat {
@@ -34,14 +35,37 @@ public class RFastDateFormat {
 
     public static String format(long time,String pattern){
         FastDateFormat fastDateFormat = FastDateFormat.getInstance(pattern);
-        
+        return fastDateFormat.format(time);
+    }
+
+    public static String format(String time,String pattern){
+        FastDateFormat fastDateFormat = FastDateFormat.getInstance(pattern);
         return fastDateFormat.format(time);
     }
 
     public static void main(String[] args) {
-        Date date = new Date();
-        System.out.println(format(date,DateFormatConstant.yyyy_MM_dd_2));
-        System.out.println(format(date.getTime(),DateFormatConstant.yyyy_MM_dd_2));
+//        Date date = new Date();
+//        System.out.println(format(date,DateFormatConstant.yyyy_MM_dd_2));
+//        System.out.println(format(date.getTime(),DateFormatConstant.yyyy_MM_dd_2));
+        String dateStr = "20220299";
+
+        FastDateFormat fastDateFormat = FastDateFormat.getInstance(DateFormatConstant.yyyyMMdd);
+
+        try {
+            Date date = fastDateFormat.parse(dateStr);
+            System.out.println(date.toString());
+            System.out.println(date.getYear());
+            System.out.println(date.getMonth());
+            System.out.println(date.getDay());
+        }catch (IllegalArgumentException  illegalArgumentException){
+            illegalArgumentException.printStackTrace();
+            System.err.println(illegalArgumentException.getMessage());
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 }
--- src/main/java/cn/cc/dawn/utils/commons/lang/RSimpleDateFormat.java
+++ src/main/java/cn/cc/dawn/utils/commons/lang/RSimpleDateFormat.java
@@ -2,6 +2,7 @@ package cn.cc.dawn.utils.commons.lang;
 
 import cn.cc.dawn.utils.constant.DateFormatConstant;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,6 +14,7 @@ public class RSimpleDateFormat {
 
     /**
      * 根据正则返回时间
+     *
      * @param regex
      * @return
      */
@@ -28,7 +30,39 @@ public class RSimpleDateFormat {
         return new SimpleDateFormat(DateFormatConstant.yyyy_MM_dd_2).format(new Date(System.currentTimeMillis()));
     }
 
+    /**
+     * 严格校验日期是否为正确格式
+     *
+     * @param dateStr 日期字符串
+     * @param pattern 正则
+     * @return 校验结果
+     */
+    public static boolean checkStrEffect(String dateStr, String pattern) {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+            // 设置严格校验， 否则 20220229 也会通过
+            simpleDateFormat.setLenient(false);
+            simpleDateFormat.parse(dateStr);
+            return true;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DateFormatConstant.yyyyMMdd);
+        String dateStr = "20220228";
+        try {
+            simpleDateFormat.setLenient(false);
+            simpleDateFormat.parse(dateStr);
+            System.out.println(true);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+
     }
 
 }
