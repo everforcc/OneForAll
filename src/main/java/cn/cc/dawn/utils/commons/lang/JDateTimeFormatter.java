4694a0e45a7db51fc03600bbbfb57a8d4f186045
everforcc <48779058+everforcc@users.noreply.github.com>
2023-04-04 17:00:01 +0800

    20230404 LocalDateTime测试

--- src/main/java/cn/cc/dawn/utils/commons/lang/JDateTimeFormatter.java
+++ src/main/java/cn/cc/dawn/utils/commons/lang/JDateTimeFormatter.java
@@ -9,6 +9,7 @@ package cn.cc.dawn.utils.commons.lang;
 
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.chrono.IsoChronology;
 import java.time.format.DateTimeFormatter;
@@ -19,6 +20,8 @@ import java.util.Date;
  * 格式化时间
  */
 public class JDateTimeFormatter {
+
+    private static LocalDateTime endTime = LocalDateTime.of(2023,5,1,0,0);
 
     public static Date parse(String dateStr, String pattern) {
         try {
@@ -35,6 +38,14 @@ public class JDateTimeFormatter {
             e.printStackTrace();
             throw new RuntimeException("日期格式化异常: " + e.getMessage());
         }
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime nowTime= LocalDateTime.now();
+        System.out.println(nowTime.isBefore(endTime));
+        LocalDateTime testTime = LocalDateTime.of(2023,5,1,0,1);
+        System.out.println(endTime.isBefore(endTime));
+        System.out.println(testTime.isBefore(endTime));
     }
 
 }
